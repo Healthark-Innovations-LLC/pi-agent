bb44d1efdcdd06f7f288610c4d5dfc16da91e868
naisanzaa <naisanza@gmail.com>
2026-06-20 03:41:11 -0700

    debug docker-out-of-docker build on windows

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ docker run --rm -it \
   --network agents \
   -p 0.0.0.0:3001:3001 \
   --memory-reservation=2g \
-  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v //var/run/docker.sock:/var/run/docker.sock \
   -v $image:/root/.pi/agent/sessions \
   -v $image-ssh:/root/.ssh \
   $image
