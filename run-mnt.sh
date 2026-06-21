51c8e7307f9daebae3a352f1729bb79d9cfd62ad
naisanzaa <naisanza@gmail.com>
2026-06-21 00:41:16 -0700

    you can mount a path for pi

---
+++ run-mnt.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+# run agent in docker
+
+image="pi-agent"
+
+set -e
+
+docker network create agents >/dev/null 2>&1 || :
+
+if [[ ! -z "$@" ]]; then
+  mnt="$@"
+else
+  mnt="."
+fi
+
+set -xe
+
+docker run --rm -it \
+  --network agents \
+  --shm-size=2g \
+  -v "$mnt":/mnt \
+  -v //var/run/docker.sock:/var/run/docker.sock \
+  -v $image-sessions:/root/.pi/agent/sessions \
+  -v $image-ssh:/root/.ssh \
+  $image
