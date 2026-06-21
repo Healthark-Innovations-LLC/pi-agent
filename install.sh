9bd8bfc6f7096cf59f8bb656c8e9a73c4b8269df
naisanzaa <naisanza@gmail.com>
2026-06-21 15:46:00 -0700

    run install.sh to put it into your local bin dir. also separte script to run with a web ui

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+# copies run.sh to your local bin
+
+set -xe 
+
+sudo cp -v run.sh /usr/bin/pi
+sudo chmod +x /usr/bin/pi
+
---
+++ run-webui.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+# run agent in docker
+
+image="pi-agent"
+
+set -e
+
+docker network create agents >/dev/null 2>&1 || :
+docker rm -f $image >/dev/null || :
+
+set -xe
+
+docker run --rm -it \
+  --name $image \
+  --network agents \
+  -p 0.0.0.0:3001:3001 \
+  --shm-size=2g \
+  -v //var/run/docker.sock:/var/run/docker.sock \
+  -v $image-sessions:/root/.pi/agent/sessions \
+  -v $image-ssh:/root/.ssh \
+  $image "$@"
+
