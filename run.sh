51f5ff25a66c3ca06d2c2ff17177595421cb1271
naisanzaa <naisanza@gmail.com>
2026-06-21 21:21:30 -0700

    [patch] agent was okay

--- run.sh
+++ run.sh
@@ -3,18 +3,17 @@
 
 image="pi-agent"
 prompt=""
-mount_path=""
+mount="."
 
 # Parse arguments
-while getopts "p:m:" opt; do
+while getopts "$@" opt; do
   case "$opt" in
     p) prompt="$OPTARG" ;;
-    m) mount_path="$OPTARG" ;;
+    m) mount="$OPTARG" ;;
     *) echo "Usage: $0 [-p prompt] [-m mount]" ; exit 1 ;;
   esac
 done
 
-# explain this
 shift $((OPTIND-1))
 
 set -e
@@ -30,6 +29,6 @@ docker run --rm -it \
   -v $image-bin:/root/.pi/agent/bin \
   -v $image-sessions:/root/.pi/agent/sessions \
   -v $image-ssh:/root/.ssh \
-  ${mount_path:+--volume $mount_path:/mnt} \
-  $image "$@"
+  -v $mount:/mnt \
+  $image "$prompt"
 
