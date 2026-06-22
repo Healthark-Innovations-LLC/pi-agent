39f7ebb83d24f41b71eeb692be1d886a9e56dbf2
naisanzaa <naisanza@gmail.com>
2026-06-22 10:25:02 -0700

    handle linux and mac install

--- install.sh
+++ install.sh
@@ -5,6 +5,5 @@ script=run.sh
 
 set -xe 
 
-sudo cp -v $script /usr/bin/pi
-sudo chmod +x /usr/bin/pi
-
+sudo cp -v $script /usr/bin/pi || sudo cp -v $script /usr/local/bin/pi
+sudo chmod +x /usr/bin/pi || sudo chmod +x /usr/local/bin/pi
