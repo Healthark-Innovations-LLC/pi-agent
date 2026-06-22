0211d9db717cdcfd8d0a6ff210cc0b10bc36b237
naisanzaa <naisanza@gmail.com>
2026-06-22 03:23:55 -0700

    [patch] default docker mount will be /root/workspace

--- run.sh
+++ run.sh
@@ -29,6 +29,6 @@ docker run --rm -it \
   -v $image-bin:/root/.pi/agent/bin \
   -v $image-sessions:/root/.pi/agent/sessions \
   -v $image-ssh:/root/.ssh \
-  -v $mount:/mnt \
+  -v $mount:/root/workspace \
   $image "$prompt"
 
